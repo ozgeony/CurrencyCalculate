458d0bebb32d3e9d1b523c21a3a530e427235cde
Özge Onay <ozge.onay@marun.edu.tr>
2017-03-24 21:10:42 +0300

    First Screen is designed.

---
+++ app/src/main/java/com/currencycalculate/CalculatorActivity.java
@@ -0,0 +1,19 @@
+package com.currencycalculate;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Özge ONAY on 22.03.2017.
+ */
+
+public class CalculatorActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator_activity);
+    }
+
+}
